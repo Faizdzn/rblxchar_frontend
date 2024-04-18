7252d8ca31f38d2ac3a631371e5fd84a32af891a
Faizdzn <masterfrostgt@gmail.com>
2024-04-19 00:54:25 +0700

    yo

--- src/views/character/mainPage.vue
+++ src/views/character/mainPage.vue
@@ -2,7 +2,7 @@
     <div class="group-flex-phone" style="gap: 50px">
       <div class="group-flex-col f-1" style="gap: 20px">
         <span class="title">Welcome to Roblox Character Downloader!</span>
-        <span class="desc">Hello visitors, welcome to the Roblox Character Downloader where you can download your Roblox avatar here. Also, if you want to support us you can head to donation page your donation will useful for our development progress. Thank you for visit our website!</span>
+        <span class="desc">Hello visitors, welcome to the Roblox Character Downloader where you can download your Roblox avatar here. I decide to remaking the old character downloader, i heard your suggestion about the keys feature are "suck". Because that, i just remaking the website without keys feature for you to enjoy character downloader without limitation. Thank you for visit our website!</span>
       </div>
       <div class="group-flex-col f-1">
         <div class="group-flex-row">
